23c0b2a78c797884a5ba5aaab6381b416a77ac87
steenj <jop.van.der.steen@rabobank.com>
2021-06-17 14:36:36 +0200

    added some comments for clearity

--- src/main/java/com/java/bankApplication/TeamRocket/Controller.java
+++ src/main/java/com/java/bankApplication/TeamRocket/Controller.java
@@ -15,29 +15,37 @@ public class Controller {
         do {
             if (choice == 1) {
                 input.nextLine();
-                login();
+                login(); // in web app, go to (or stay) login screen
             } else if (choice == 2) {
                 input.nextLine();
-                createAccount();
+                createAccount(); // in web app, go to create account screen
             } else {
                 informationLogger.invalidInput();
-                choice = input.nextInt();
+                choice = input.nextInt(); // in web app, show an error text and clear fields.
             }
         } while (choice < 1 || choice > 2);
 
     }
 
     public void login() {
+        // retrieve users information
         informationLogger.printLogin();
         informationLogger.requestUsername();
         String username = input.nextLine();
         informationLogger.requestPassword();
         String password = input.nextLine();
 
+        // logic to check if user exists should come here
+
+        // if user exist, go to overview page
+
+        // if not, throw an error/ask to login again.
+
 
     }
 
     public void createAccount() {
+        // retrieve users information
         informationLogger.printCreateAccount();
         System.out.print("first name:");
         String firstName = input.nextLine();
@@ -47,6 +55,8 @@ public class Controller {
         String email = input.nextLine();
         System.out.print("IBAN Number:");
         String ibanNumber = input.nextLine();
+
+        // create an account for this person with username and password
         createLogin();
     }
 
