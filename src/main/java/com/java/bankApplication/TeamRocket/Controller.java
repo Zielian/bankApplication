39f77ca38e2247b69fe47050d60a5892fe4c088a
steenj <jop.van.der.steen@rabobank.com>
2021-06-17 08:40:58 +0200

    Created login logic, not linked to actual customers yet.

--- src/main/java/com/java/bankApplication/TeamRocket/Controller.java
+++ src/main/java/com/java/bankApplication/TeamRocket/Controller.java
@@ -1,4 +1,73 @@
 package com.java.bankApplication.TeamRocket;
 
+import java.util.Scanner;
+
 public class Controller {
+
+    static private Printer informationLogger = new Printer();
+    static private Scanner input = new Scanner(System.in);
+
+
+    public void loginOption() {
+        informationLogger.printLoginOption();
+        int choice = input.nextInt();
+
+        do {
+            if (choice == 1) {
+                login();
+            } else if (choice == 2) {
+                createAccount();
+            } else {
+                informationLogger.invalidInput();
+                choice = input.nextInt();
+            }
+        } while (choice < 1 || choice > 2);
+
+    }
+
+    public void login() {
+        informationLogger.printLogin();
+        input.nextLine();
+        System.out.print("username:");
+        String username = input.nextLine();
+        System.out.print("password:");
+        String password = input.nextLine();
+
+
+    }
+
+    public void createAccount() {
+        informationLogger.printCreateAccount();
+        input.nextLine();
+        System.out.print("first name:");
+        String firstName = input.nextLine();
+        System.out.print("last name:");
+        String lastName = input.nextLine();
+        System.out.print("email:");
+        String email = input.nextLine();
+        System.out.print("IBAN Number:");
+        String ibanNumber = input.nextLine();
+        createLogin();
+    }
+
+    public void createLogin() {
+        informationLogger.printCreateLogin();
+        System.out.print("username:");
+        String username = input.nextLine();
+        // check if username already exists
+        // if so, return an error
+        System.out.print("password:");
+        String password = input.nextLine();
+        System.out.print("confirm password:");
+        do {
+            if (!input.nextLine().equals(password)) {
+                informationLogger.noMatchingPasswords();
+                createLogin();
+            } else {
+                informationLogger.printSuccesfulAccountCreation();
+            }
+
+        } while (!input.nextLine().equals(password));
+    }
 }
+
--- src/main/java/com/java/bankApplication/TeamRocket/Printer.java
+++ src/main/java/com/java/bankApplication/TeamRocket/Printer.java
@@ -1,4 +1,41 @@
 package com.java.bankApplication.TeamRocket;
 
+import java.util.Scanner;
+
 public class Printer {
+
+    public void printLoginOption(){
+        System.out.println("Welcome, please pick one of the following:");
+        System.out.println("1) Login");
+        System.out.println("2) Create new account");
+    }
+
+    public void printLogin(){
+        System.out.println("Please provide your username and password.");
+    }
+
+    public void printCreateAccount(){
+        System.out.println("Thank you for signing up with the Rabobank");
+        System.out.println("Please provide the following details:");
+    }
+
+    public void printCreateLogin() {
+        System.out.println("In order to be able to login in the future you also need to create a username and password.");
+    }
+
+    public void printSuccesfulAccountCreation() {
+        System.out.println("Thank you for signing up.");
+        System.out.println("You will now be redirected to the login page.");
+    }
+
+    public void invalidInput() {
+        System.out.println("That's not a valid option.");
+        System.out.println("Please pick again.");
+    }
+
+    public void noMatchingPasswords() {
+        System.out.println("Sorry, these passwords don't match.");
+        System.out.println("Please try again.");
+    }
+
 }
